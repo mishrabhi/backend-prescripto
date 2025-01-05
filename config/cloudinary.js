50fedbe0cda450dfcf89d49d954022e5f4e9b5b7
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-06 01:01:53 +0530

    created routes and added doctor

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,7 @@ const cloudinary = require("cloudinary").v2;
 
 const connectCloudinary = async () => {
   cloudinary.config({
-    cloud_name: process.env.CLOUDDINARY_NAME,
+    cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_SECRET_KEY,
   });
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,10 @@
 const validator = require("validator"); //validates email
 const bcrypt = require("bcrypt");
+const cloudinary = require("cloudinary").v2;
+const doctorModel = require("../models/doctorModel");
 
 //API for adding doctors
-const addDoctor = async () => {
+const addDoctor = async (req, res) => {
   try {
     const {
       name,
@@ -62,7 +64,35 @@ const addDoctor = async () => {
     //hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-  } catch (error) {}
+
+    //upload image to cloudinary
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+
+    //Collect doctor data
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+    };
+
+    //save doctor
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+    res.json({ success: true, message: "Doctor added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 module.exports = addDoctor;
