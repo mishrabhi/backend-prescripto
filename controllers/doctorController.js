db1ce955e8f494372a7f756948da74fbce8c9520
mishrabhi <asmyselfabhishek00@gmail.com>
2025-01-07 16:23:13 +0530

    added change availability of doctors

---
+++ controllers/doctorController.js
@@ -0,0 +1,18 @@
+const doctorModel = require("../models/doctorModel");
+
+//Change Availability of doctors
+const changeAvailability = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const docData = await doctorModel.findById(docId);
+    await doctorModel.findByIdAndUpdate(docId, {
+      available: !docData.available,
+    });
+    res.json({ success: true, message: "Availability changed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+module.exports = changeAvailability;
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -6,11 +6,13 @@ const {
 } = require("../controllers/adminController");
 const upload = require("../middlewares/multer");
 const authAdmin = require("../middlewares/authAdmin.js");
+const changeAvailability = require("../controllers/doctorController.js");
 
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/login", loginAdmin);
 adminRouter.post("/all-doctors", authAdmin, allDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
 
 module.exports = adminRouter;
